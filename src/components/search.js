f1cd17de2a4ef1d9cbb9a43b94dfd4d8cab97e63
Shylmysten <TBoswell@kc.rr.com>
2018-08-06 14:18:16 -0500

    feat: add state/props to input

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import BookGrid from './BookGrid'
 
 class Search extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
 
   render() {
+
+    const books = Object.keys(this.props.shelves).map(key => this.props.shelves[key])
+
     return(
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,12 +30,18 @@ class Search extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          {/* <BookGrid books={this.props.books} /> */}
+        {JSON.stringify(this.state)}
         </div>
       </div>
 
