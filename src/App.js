d257a82162aaae6425a1cb8d6af9ea4e3aca3513
Shylmysten <TBoswell@kc.rr.com>
2018-08-07 12:15:32 -0500

    feat: add none category
    category serves no purpose other than to handle errors from moving a to
    or from a shelf that didn't exist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { debounce } from 'lodash'
 import { Route } from 'react-router-dom'
 import Library from './components/Library'
 import Search from './components/search'
 import * as BooksAPI from './utils/BooksAPI'
+import nocover from './icons/nocover.jpg'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -15,13 +17,16 @@ class BooksApp extends React.Component {
        * users can use the browser's back and forward buttons to navigate between
        * pages, as well as provide a good URL they can bookmark and share.
        */
+      query: '',
+      searchResults: [],
       shelves: {
         currentlyReading: [],
         wantToRead: [],
-        read: []
+        read: [],
+        none: []
       }
     }
-
+    this.updateQuery = debounce(this.updateQuery, 500);
     this.handleChange = this.handleChange.bind(this)
   }
 
@@ -109,6 +114,7 @@ class BooksApp extends React.Component {
     e.persist();
     BooksAPI.update(book, e.target.value);
     book.shelf= e.target.value;
+
     this.setState({shelves: {
       ...this.state.shelves,
         [oldShelf]: [...this.state.shelves[oldShelf]].filter( b => b.id !== book.id),
@@ -134,6 +140,75 @@ class BooksApp extends React.Component {
 
 
 
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    });
+    this.search(query);
+  }
+
+
+
+  search = (query) => {
+
+    if (query === '') {
+      this.setState({
+        searchResults: []
+      });
+      return;
+    }
+
+    BooksAPI.search(query)
+    .then((books) => {
+
+      let bookshelves = this.state.shelves.currentlyReading.concat(this.state.shelves.wantToRead, this.state.shelves.read);
+
+      this.setState({ searchResults: [] })
+
+      books.map(book => {
+
+        if (!('shelf' in book)) {
+          book.shelf = 'none'
+        }
+
+        if (!('authors' in book)) {
+          book.authors = ' '
+        }
+
+        if (!('imageLinks' in book)) {
+          book.imageLinks = {thumbnail: nocover}
+        }
+
+        bookshelves.map(item => {
+
+          if (item.id === book.id) {
+            console.log('We have a Match');
+            book.shelf= item.shelf;
+          }
+
+        })
+        this.setState({
+          searchResults: this.state.searchResults.concat(book)
+        },
+        // () => console.log(book, this.state.searchResults)
+       )
+
+
+    })
+    }).catch( error => {
+      console.log(error.message);
+      console.log(`${error.message}`)
+      if (error.hasOwnProperty('message') && error.message.includes('books.map is not a function')) {
+        alert('Your search was invalid or returned no results, please try again!');
+      }
+
+
+    })
+  }
+
+
+
+
   render() {
     return (
       <div className="app">
@@ -147,6 +222,8 @@ class BooksApp extends React.Component {
           <Search
             shelves={this.state.shelves}
             handleChange={this.handleChange}
+            updateQuery={this.updateQuery}
+            searchResults={this.state.searchResults}
           />
         )}/>
       </div>
