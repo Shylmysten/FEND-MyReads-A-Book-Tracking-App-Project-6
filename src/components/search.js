00abddcf36d22f533a5799edae9eb12b3e5d55dd
Shylmysten <TBoswell@kc.rr.com>
2018-08-08 09:54:29 -0500

    feat: add clearQuery

--- src/components/search.js
+++ src/components/search.js
@@ -8,25 +8,15 @@ class Search extends Component {
 
   render() {
 
-    const { shelves, query, searchResults } = this.props
-    const books = searchResults
-
+    const { searchResults } = this.props
 
     return(
       <div className="search-books">
         <div className="search-books-bar">
           <button className="close-search">
-            <Link to='/'>Close</Link>
+            <Link to='/' onClick={()=> this.props.clearQuery()}>Close</Link>
           </button>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input
               type="text"
               placeholder="Search by title or author"
